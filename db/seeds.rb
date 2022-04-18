44706ab3e977ff90f98c4f79560957d604e760f4
ben <bjelinek42@gmail.com>
2022-04-18 09:27:34 -0600

    user, group, restaurant seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# user = User.new(name: "Ben", email: "ben@gmail.com", password_digest: "password", group_id: 2)
+# user.save
+# user = User.new(name: "Katrina", email: "katrina@gmail.com", password_digest: "password", group_id: 2)
+# user.save
+# user = User.new(name: "Howie", email: "howie@gmail.com", password_digest: "password", group_id: 2)
+# user.save
+# user = User.new(name: "Jimmy", email: "jimmy@gmail.com", password_digest: "password", group_id: 1)
+# user.save
+# user = User.new(name: "Drew", email: "drew@gmail.com", password_digest: "password", group_id: 1)
+# user.save
+# user = User.new(name: "Travis", email: "travis@gmail.com", password_digest: "password", group_id: 3)
+# user.save
+# user = User.new(name: "Erica", email: "erica@gmail.com", password_digest: "password", group_id: 3)
+# user.save
+# user = User.new(name: "Zac", email: "zac@gmail.com", password_digest: "password", group_id: 1)
+# user.save
+# user = User.new(name: "Steve", email: "steve@gmail.com", password_digest: "password", group_id: 3)
+# user.save
+# user = User.new(name: "Rachel", email: "rachel@gmail.com", password_digest: "password", group_id: 3)
+# user.save
+# user = User.new(name: "Brian", email: "brian@gmail.com", password_digest: "password", group_id: 1)
+# user.save
+
+# group = Group.new(name: "Ultimate")
+# group.save
+# group = Group.new(name: "Home")
+# group.save
+# group = Group.new(name: "Party")
+# group.save
+
+# restaurant = Restaurant.new(name: "Hacienda", cuisines: "Mexican", address: "123 Fake St.", image: "https://bloximages.newyork1.vip.townnews.com/gazette.com/content/tncms/assets/v3/editorial/e/d8/ed8f1a46-62e8-11e9-acb5-57e3096b1118/5cba3ba360674.image.jpg")
+# restaurant.save
+# restaurant = Restaurant.new(name: "Mod Pizza", cuisines: "Pizza", address: "462 Second St.", image: "https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/MOD_Pizza_logo.svg/1200px-MOD_Pizza_logo.svg.png")
+# restaurant.save
+# restaurant = Restaurant.new(name: "Great Wall", cuisines: "Chinese", address: "9284 North Ave", image: "http://file1.chinesemenu.com/101326954/201211271037371.jpg")
+# restaurant.save
+# restaurant = Restaurant.new(name: "Capriotti's", cuisines: "Sandwhiches", address: "842 Washington St.", image: "https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_1200,h_800,f_auto,fl_lossy,q_80,c_fit/tpzxqqylcvt4rha0hrcs")
+# restaurant.save
+# restaurant = Restaurant.new(name: "Texas Roadhouse", cuisines: "American", address: "898 Texas Ave", image: "https://media-cdn.tripadvisor.com/media/photo-s/09/19/2c/ac/texas-roadhouse.jpg")
+# restaurant.save
+# restaurant = Restaurant.new(name: "Chef Bob's Lobstah Trap", cuisines: "Cajun", address: "592 South St.", image: "https://d3hbe0kmbam4a5.cloudfront.net/photos/b0201ba4-558f-4b55-8b12-fd4de5c75497.jpg")
+# restaurant.save
