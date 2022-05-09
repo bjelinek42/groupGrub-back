56cff9b86e84201330a4070d9d710de35d38cb7e
ben <bjelinek42@gmail.com>
2022-05-09 14:17:29 -0600

    no multiple active votes

--- app/controllers/vote_restaurants_controller.rb
+++ app/controllers/vote_restaurants_controller.rb
@@ -1,46 +1,50 @@
 class VoteRestaurantsController < ApplicationController
   def create
-    group = current_user.group
-    votes = []
-    current_user.group.users.each do |user|
-      user.restaurants.each do |restaurant|
-        votes << restaurant
+    if VoteRestaurant.where(group_id: current_user.group_id, active: true) != []
+      render json: {message: "only one vote allowed at a time. Please finish current vote"}
+    else
+      group = current_user.group
+      votes = []
+      current_user.group.users.each do |user|
+        user.restaurants.each do |restaurant|
+          votes << restaurant
+        end
       end
-    end
-    tally = {}
-    votes.each do |restaurant|
-      if tally[restaurant.id] == nil
-        tally[restaurant.id] = 0
+      tally = {}
+      votes.each do |restaurant|
+        if tally[restaurant.id] == nil
+          tally[restaurant.id] = 0
+        end
+        tally[restaurant.id] += 1
       end
-      tally[restaurant.id] += 1
-    end
-    top_three = []
-    top_three = tally.sort_by { |_k, v| -v }.first(3).map(&:first)
-    # i = 0
-    # 3.times do
-    #   top_three << tally.max_by{ |_k,v| v}[i]
-    #   i += 1
-    # end
-    sanity = []
-    j = 0
-    group.users.length.times do
-      i = 0
-      user_id = group.users[j].id
-      3.times do
-        vr = VoteRestaurant.new(
-          group_id: current_user.group_id,
-          restaurant_id: top_three[i],
-          active: true,
-          vote: false,
-          user_id: user_id
-        )
-        vr.save
-        i += 1
-        sanity << vr
+      top_three = []
+      top_three = tally.sort_by { |_k, v| -v }.first(3).map(&:first)
+      # i = 0
+      # 3.times do
+      #   top_three << tally.max_by{ |_k,v| v}[i]
+      #   i += 1
+      # end
+      sanity = []
+      j = 0
+      group.users.length.times do
+        i = 0
+        user_id = group.users[j].id
+        3.times do
+          vr = VoteRestaurant.new(
+            group_id: current_user.group_id,
+            restaurant_id: top_three[i],
+            active: true,
+            vote: false,
+            user_id: user_id
+          )
+          vr.save
+          i += 1
+          sanity << vr
+        end
+        j += 1
       end
-      j += 1
+      render json: sanity
     end
-    render json: sanity
   end
 
   def update
@@ -54,13 +58,13 @@ class VoteRestaurantsController < ApplicationController
     user = current_user
     restaurants = []
     vote_hash = {}
-    vr = VoteRestaurant.where(user_id: 1) #current_user.id
+    vr = VoteRestaurant.where(user_id: current_user.id)
     vr.each do |option|
       r = Restaurant.find(option.restaurant_id)
       vote_hash = {}
       vote_hash[option.id] = r
       restaurants << vote_hash
     end
-    render json: restaurants
+    render json: vr
   end
 end
