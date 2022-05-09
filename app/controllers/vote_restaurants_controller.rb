93df682b6f698abe23b7de1651409874ad704745
ben <bjelinek42@gmail.com>
2022-05-09 12:18:34 -0600

    vote_restaurant create action for voting

--- app/controllers/vote_restaurants_controller.rb
+++ app/controllers/vote_restaurants_controller.rb
@@ -1,6 +1,44 @@
 class VoteRestaurantsController < ApplicationController
   def create
-    vr = Vote_restaurant.new
-    render json: {message: "hey"}
+    group = current_user.group
+    votes = []
+    current_user.group.users.each do |user|
+      user.restaurants.each do |restaurant|
+        votes << restaurant
+      end
+    end
+    tally = {}
+    votes.each do |restaurant|
+      if tally[restaurant.id] == nil
+        tally[restaurant.id] = 0
+      end
+      tally[restaurant.id] += 1
+    end
+    top_three = []
+    i = 0
+    3.times do
+      top_three << tally.max_by{ |_k,v| v}[i]
+      i += 1
+    end
+    sanity = []
+    j = 0
+    group.users.length.times do
+      i = 0
+      user_id = group.users[j].id
+      3.times do
+        vr = VoteRestaurant.new(
+          group_id: current_user.group_id,
+          restaurant_id: top_three[i],
+          active: true,
+          vote: false,
+          user_id: user_id
+        )
+        i += 1
+        sanity << vr
+      end
+      j += 1
+    end
+    render json: sanity
   end
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,30 @@
 class Group < ApplicationRecord
   has_many :users
+  has_many :restaurant_users, through: :users
   belongs_to :vote_restaurant
+
+  def create_vote
+    group = current_user.group
+    votes = []
+    current_user.group.users.each do |user|
+      user.restaurants.each do |restaurant|
+        votes << restaurant
+      end
+    end
+    tally = {}
+    votes.each do |restaurant|
+      if tally[restaurant.id] == nil
+        tally[restaurant.id] = 0
+      end
+      tally[restaurant.id] += 1
+    end
+    top_three = []
+    i = 0
+    3.times do
+      top_three << tally.max_by{ |_k,v| v}[i]
+      i += 1
+    end
+    render json: top_three
+  end
+  
 end
