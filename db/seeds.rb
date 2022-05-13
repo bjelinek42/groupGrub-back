becf212c467969508fee56ff0405f57d50b96ce7
ben <bjelinek42@gmail.com>
2022-05-12 19:47:05 -0600

    new seed file, seed dump gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,93 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-# user = User.new(name: "Ben", email: "ben@gmail.com", password: "password", group_id: 2)
+# user = User.new(name: "Jimmy", email: "jimmy@email.com", password: "password", group_id: 1)
 # user.save
-# user = User.new(name: "Katrina", email: "katrina@gmail.com", password: "password", group_id: 2)
+# user = User.new(name: "Drew", email: "drew@email.com", password: "password", group_id: 1)
 # user.save
-# user = User.new(name: "Howie", email: "howie@gmail.com", password: "password", group_id: 2)
+# user = User.new(name: "Zac", email: "zac@email.com", password: "password", group_id: 1)
 # user.save
-# user = User.new(name: "Jimmy", email: "jimmy@gmail.com", password: "password", group_id: 1)
+# user = User.new(name: "Travis", email: "travis@email.com", password: "password", group_id: 2)
 # user.save
-# user = User.new(name: "Drew", email: "drew@gmail.com", password: "password", group_id: 1)
+# user = User.new(name: "Erica", email: "erica@email.com", password: "password", group_id: 2)
 # user.save
-# user = User.new(name: "Travis", email: "travis@gmail.com", password: "password", group_id: 3)
+# user = User.new(name: "Steve", email: "steve@email.com", password: "password", group_id: 2)
 # user.save
-# user = User.new(name: "Erica", email: "erica@gmail.com", password: "password", group_id: 3)
+# user = User.new(name: "Rachel", email: "rachel@email.com", password: "password", group_id: 2)
 # user.save
-# user = User.new(name: "Zac", email: "zac@gmail.com", password: "password", group_id: 1)
+# user = User.new(name: "Sam", email: "sam@email.com", password: "password", group_id: 2)
 # user.save
-# user = User.new(name: "Steve", email: "steve@gmail.com", password: "password", group_id: 3)
+# user = User.new(name: "Todd", email: "todd@email.com", password: "password", group_id: 2)
 # user.save
-# user = User.new(name: "Rachel", email: "rachel@gmail.com", password: "password", group_id: 3)
+# user = User.new(name: "Mary", email: "mary@email.com", password: "password", group_id: 2)
 # user.save
-# user = User.new(name: "Brian", email: "brian@gmail.com", password: "password", group_id: 1)
+# user = User.new(name: "Shannon", email: "shannon@email.com", password: "password", group_id: 2)
+# user.save
+# user = User.new(name: "Logan", email: "logan@email.com", password: "password", group_id: 3)
+# user.save
+# user = User.new(name: "John", email: "john@email.com", password: "password", group_id: 3)
+# user.save
+# user = User.new(name: "Elaine", email: "elaine@email.com", password: "password", group_id: 3)
+# user.save
+# user = User.new(name: "Ashwin", email: "ashwin@email.com", password: "password", group_id: 3)
+# user.save
+# user = User.new(name: "Kristin", email: "kristin@email.com", password: "password", group_id: 3)
+# user.save
+# user = User.new(name: "Ben", email: "ben@email.com", password: "password", group_id: 3)
 # user.save
 
 # group = Group.new(name: "Ultimate")
 # group.save
-# group = Group.new(name: "Home")
-# group.save
 # group = Group.new(name: "Party")
 # group.save
-
-# restaurant = Restaurant.new(name: "Hacienda", cuisines: "Mexican", address: "123 Fake St.", image: "https://bloximages.newyork1.vip.townnews.com/gazette.com/content/tncms/assets/v3/editorial/e/d8/ed8f1a46-62e8-11e9-acb5-57e3096b1118/5cba3ba360674.image.jpg")
-# restaurant.save
-# restaurant = Restaurant.new(name: "Mod Pizza", cuisines: "Pizza", address: "462 Second St.", image: "https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/MOD_Pizza_logo.svg/1200px-MOD_Pizza_logo.svg.png")
-# restaurant.save
-# restaurant = Restaurant.new(name: "Great Wall", cuisines: "Chinese", address: "9284 North Ave", image: "http://file1.chinesemenu.com/101326954/201211271037371.jpg")
-# restaurant.save
-# restaurant = Restaurant.new(name: "Capriotti's", cuisines: "Sandwhiches", address: "842 Washington St.", image: "https://media-cdn.grubhub.com/image/upload/d_search:browse-images:default.jpg/w_1200,h_800,f_auto,fl_lossy,q_80,c_fit/tpzxqqylcvt4rha0hrcs")
-# restaurant.save
-# restaurant = Restaurant.new(name: "Texas Roadhouse", cuisines: "American", address: "898 Texas Ave", image: "https://media-cdn.tripadvisor.com/media/photo-s/09/19/2c/ac/texas-roadhouse.jpg")
-# restaurant.save
-# restaurant = Restaurant.new(name: "Chef Bob's Lobstah Trap", cuisines: "Cajun", address: "592 South St.", image: "https://d3hbe0kmbam4a5.cloudfront.net/photos/b0201ba4-558f-4b55-8b12-fd4de5c75497.jpg")
-# restaurant.save
-
-# ru = RestaurantUser.new(restaurant_id: 1, user_id: 6)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 1, user_id: 7)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 2, user_id: 1)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 2, user_id: 2)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 3, user_id: 8)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 3, user_id: 4)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 4, user_id: 5)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 4, user_id: 7)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 1, user_id: 3)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 2, user_id: 6)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 4, user_id: 4)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 5, user_id: 6)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 5, user_id: 3)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 6, user_id: 6)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 6, user_id: 9)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 6, user_id: 2)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 5, user_id: 4)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 4, user_id: 10)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 4, user_id: 11)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 2, user_id: 11)
-# ru.save
-# ru = RestaurantUser.new(restaurant_id: 1, user_id: 11)
-# ru.save
+# group = Group.new(name: "Lyft")
+# group.save
+# group = Group.new(name: "Home")
+# group.save
