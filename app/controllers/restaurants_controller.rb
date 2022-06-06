07d9448bb1d86e0053a1f05495b28f746fe61876
ben <bjelinek42@gmail.com>
2022-06-06 12:28:28 -0600

    change requests for restaurants to 50

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -47,7 +47,7 @@ class RestaurantsController < ApplicationController
     request["content-type"] = 'application/x-www-form-urlencoded'
     request["X-RapidAPI-Host"] = 'worldwide-restaurants.p.rapidapi.com'
     request["X-RapidAPI-Key"] = ENV["RESTAURANT_API"]
-    request.body = "language=en_US&limit=1000&location_id=#{location_id}&currency=USD"
+    request.body = "language=en_US&limit=50&location_id=#{location_id}&currency=USD"
 
     response = http.request(request)
 
