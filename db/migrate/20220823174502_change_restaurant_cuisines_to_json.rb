7b6faf9cd40685070cefd03b6f67fc944da89371
ben <bjelinek42@gmail.com>
2022-08-23 12:15:48 -0600

    change cuisine data type to json

--- db/migrate/20220823174502_change_restaurant_cuisines_to_json.rb
+++ db/migrate/20220823174502_change_restaurant_cuisines_to_json.rb
@@ -2,8 +2,4 @@ class ChangeRestaurantCuisinesToJson < ActiveRecord::Migration[7.0]
   def change 
     remove_column :restaurants, :cuisines
   end
-
-  def change
-    add_column :restaurants, :cuisines, :json
-  end
 end
